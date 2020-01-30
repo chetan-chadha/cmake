bbc3039938478de7934000027a62b731ea378cdc
chetan chadha <chetanchadha@curlanalytics.com>
2020-01-30 19:25:18 +0530

    added libraries

--- build/say-hello/cmake_install.cmake
+++ build/say-hello/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/chetan/udacity/cmake_tutorial
+# Install script for directory: /home/chetan/udacity/cmake_tutorial/say-hello
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,20 +37,3 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("/home/chetan/udacity/cmake_tutorial/build/say-hello/cmake_install.cmake")
-  include("/home/chetan/udacity/cmake_tutorial/build/hello-exe/cmake_install.cmake")
-
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/chetan/udacity/cmake_tutorial/build/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
