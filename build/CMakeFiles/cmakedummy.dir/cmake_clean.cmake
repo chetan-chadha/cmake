2394218c3e95c4fbc68def9152bb2cc5e99c91c7
chetan chadha <chetanchadha@curlanalytics.com>
2020-01-30 18:43:14 +0530

    first_commit

---
+++ build/CMakeFiles/cmakedummy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmakedummy.dir/main.cpp.o"
+  "cmakedummy"
+  "cmakedummy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmakedummy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
